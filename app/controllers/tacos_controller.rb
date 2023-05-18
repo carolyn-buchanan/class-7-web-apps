265ff7c2ced5b4412e81d7530618e09a8eb8c69c
Carolyn Buchanan <jlq5921@kellogg.northwestern.edu>
2023-05-18 03:43:38 +0000

    tacos!

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -2,6 +2,9 @@ class TacosController < ApplicationController
 
   def index
     # render tacos/index view
+    @fillings = ["Carnitas", "Al Pastor", "Steak", "Fish", "Veggie"]
+
+    render :template=> "tacos/index"
   end
 
 end
